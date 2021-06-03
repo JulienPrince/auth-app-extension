1099ad04ce962e438d7ecdb68ffe77651ba47ea6
Julien Prince <prince.julienh@gmail.com>
2021-06-03 09:26:12 +0300

    up route

--- app-extension/src/boot/register.js
+++ app-extension/src/boot/register.js
@@ -1,6 +1,27 @@
 import { boot } from "quasar/wrappers";
 import VuePlugin from "quasar-ui-plugin.auth/src/index.js";
 
-export default boot(({ app }) => {
+export default boot(({ app, router }) => {
   app.use(VuePlugin);
+
+  let { routes } = router.options;
+  let routeData = routes.find((r) => r.path === "/");
+
+  const currentRoutes = routeData.children.map((route) => route.path);
+
+  const newRoutes = [
+    {
+      path: "/login",
+      component: () => import("quasar-ui-plugin.auth/src/components/Auth"),
+    },
+  ];
+
+  routeData.children = [];
+  for (let route of newRoutes) {
+    if (!currentRoutes.includes(route.path)) {
+      routeData.children.push(route);
+    }
+  }
+  console.log("madalo ato routeData", routeData);
+  router.addRoute(routeData);
 });
